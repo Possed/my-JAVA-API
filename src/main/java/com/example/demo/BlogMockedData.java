1f32a100d0aa1928ab86731551311812b87f283b
Possed <r0xana.pirvu@yahoo.com>
2018-07-13 18:01:38 +0100

    Created a Singleton for BlogMockedData
    created a static method that returns checks if the class is already
    instatiated and returns the existing instance else it creates a new
    instance of that class and returns it

--- src/main/java/com/example/demo/BlogMockedData.java
+++ src/main/java/com/example/demo/BlogMockedData.java
@@ -1,4 +1,16 @@
 package com.example.demo;
 
+import java.util.List;
+
 public class BlogMockedData {
+    //list of blog posts
+    private List<Blog> blogs;
+
+    private static BlogMockedData instance = null;
+    public static BlogMockedData getInstance(){
+        if(instance == null){
+            instance = new BlogMockedData();
+        }
+        return instance;
+    }
 }
