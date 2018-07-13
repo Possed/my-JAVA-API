7692d45efa8e4fdc0a71688517e2b0763d52fc3a
Possed <r0xana.pirvu@yahoo.com>
2018-07-13 16:10:29 +0100

    Replace demo method with index method
    index method handles all the request to the root URL

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,19 +1,16 @@
 package com.example.demo.controller;
 
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@Controller
-@RequestMapping("/demo")
+@RestController
 public class DemoController {
 
-    @ResponseBody
-    @RequestMapping(method = GET, produces = "application/json")
-    public String demo() {
-        return "{\"hello\":\"world\"}";
+    @RequestMapping("/")
+    public String index() {
+        return "GZ";
     }
 }
