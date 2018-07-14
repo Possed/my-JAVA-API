d76c44217931f3d4d38c2da53cf80e68b5966c6a
Possed <r0xana.pirvu@yahoo.com>
2018-07-14 22:55:10 +0100

    update controller with post request that searches for blog posts nd returns all that matches the search text

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -3,11 +3,10 @@ package com.example.demo.controller;
 
 import com.example.demo.Blog;
 import com.example.demo.BlogMockedData;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -25,5 +24,11 @@ public class DemoController {
     public Blog show(@PathVariable String id){
         int blogId = Integer.parseInt(id);
         return blogMockedData.getBlogById(blogId);
+    }
+
+    @PostMapping("/blog/search")
+    public List<Blog> search(@RequestBody Map<String, String> body){
+        String searchTerm = body.get("text");
+        return blogMockedData.searchBlogs(searchTerm);
     }
 }
